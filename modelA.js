5fce95c11f2fff3c9bc57fe8a2f341764690ff39
Rory Magee <roryemagee@gmail.com>
2021-10-24 20:39:29 -0600

    Fix file location

---
+++ modelA.js
@@ -0,0 +1,10 @@
+var airport = {
+  name: "Santa Ana International Airport",
+  type: "International",
+  maxOccupancy: 300,
+  flights: 12,
+  operating: true,
+  popular: true,
+  terminals: ['A', 'B', 'C'],
+  terminalFlights: [4, 4, 4],
+}
---
+++ modelB.js
@@ -0,0 +1,10 @@
+var vehicle = {
+  make: "Tesla",
+  model: "X",
+  power: 100,
+  maxSpeed: 200,
+  electric: true,
+  cool: true,
+  passengeryOccupancyName: ['Rory','empty','empty','empty','empty'],
+  passengerOccupancyBinary: [1, 0, 0, 0, 0],
+}
